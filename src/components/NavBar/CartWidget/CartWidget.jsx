75dc80f56d969afe2d2b25c0185ce7bf19d09e68
ELIANAMEZA <elianameza88@gmail.com>
2022-03-28 10:35:04 -0300

    'modificaciones'

--- src/components/NavBar/CartWidget/CartWidget.jsx
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -5,36 +5,16 @@ import { Link } from 'react-router-dom'
 import { CartContext } from '../../../context/cartContext'
 
 export const CartWidget = () => {
-    const {cart, totalCantidad} = useContext(CartContext)
+    const {totalCantidad} = useContext(CartContext)
 
     return (
         <div id='cart'>
             <Link to={'/cart'} id='icono'>
                 
                 <img src={cartIcon} id='carrito' />
-                <div className='total'>{totalCantidad ()}</div>
+                {totalCantidad() > 0 ? <div className='total'>{totalCantidad ()}</div> : <></> }
                 
             </Link>
         </div>
     )
 }
-
-// function CartWidget () {
-//     const { cart, totalCantidad} = useContext()
-//     return(
-//         <div id='cart'>
-//             <Link to={'/cart'} id='icono'>
-//                 {cart.length === 0}
-//                  <img src={cartIcon} id='carrito' />
-//                  :
-//                  <>
-//                     <img src={cartIcon} id='carrito' />
-//                     <div>{totalCantidad}</div>
-                    
-//                  </>
-//              </Link>
-//          </div>
-//     )
-// }
-
-// export default CartWidget
