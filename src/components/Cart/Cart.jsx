d4b38d7eced5d5c1253ca1f0c293615cc1e6f11e
ELIANAMEZA <elianameza88@gmail.com>
2022-03-30 23:11:21 -0300

    'modificaciones'

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../context/cartContext';
 import { Link } from 'react-router-dom'
 import './Cart.css'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  // query,
+  // documentId,
+  // where,
+  // getDocs,
+  // writeBatch,
+} from "firebase/firestore";
+
 
 
 export const Cart = () => {
