ffeae3650cdeb43d46d9a577aa0bc556e6975692
ELIANAMEZA <elianameza88@gmail.com>
2022-03-31 20:57:46 -0300

    'buyer

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,24 +3,66 @@ import { CartContext } from '../../context/cartContext';
 import { Link } from 'react-router-dom'
 import './Cart.css'
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  // query,
-  // documentId,
-  // where,
-  // getDocs,
-  // writeBatch,
+  addDoc, 
+  collection, 
+  // doc, 
+  // documentId, 
+  // getDocs, 
+  getFirestore, 
+  // query, 
+  // updateDoc, 
+  // where, 
+  // writeBatch 
 } from "firebase/firestore";
 
-
-
 export const Cart = () => {
+  const [id, setId] = useState("")
+  const handleChange = (e) => {
+    setDataForm({
+      ...dataForm,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const [dataForm, setDataForm] = useState({
+    email: "",
+    name: "",
+    phone: "",
+  })
+
   const {cart, vaciarCarrito, removeItem, totalCompra} = useContext(CartContext)
+
+  const generarOrden = async () => {
+    let orden = {}
+    orden.buyer = dataForm
+    orden.total = totalCompra()
+    orden.fecha = new Date()
+    orden.items = cart.map((cartItem) => {
+      const id = cartItem.id
+      const nombre = cartItem.title
+      const precio = cartItem.price * cartItem.cantidad
+     
+      return { id, nombre, precio}
+    })
+
+    console.log(orden)
+
+    const db = getFirestore()
+    const queryColection = collection(db, 'orders')
+    addDoc(queryColection, orden).then((resp) => setId(resp.id))
+      .catch((err) => console.error(err))
+      .finally(() => console.log("terminado"))
+    
+  }
+
+  console.log(cart)
+
+ 
 
   return(
    
     <div className='div-return'> 
+  
       {cart.length === 0 ? 
             <>
                 <h2>Tu carrito está vacío</h2>
@@ -29,8 +71,8 @@ export const Cart = () => {
                 </Link>
             </>
             :
-            <>{
-                cart.map(product => 
+            <>
+            {cart.map(product => 
                 <div key={product.id} className='div-cart'>
                     <div className='portada-cart'>
                       <img src={product.imgUrl} alt={product.title} className='img-cart'/>
@@ -41,7 +83,7 @@ export const Cart = () => {
                       <h4>cantidad:{product.cantidad}</h4>
                       <button onClick={() => removeItem(product.id)}> X </button>
                     </div>
-                  
+                  {id.length !== '' && `el id de la compra es: ${id}`}
                 </div>)
               }
 
@@ -51,11 +93,12 @@ export const Cart = () => {
                   <Link to={'/'} >
                       <button>Seguir comprando</button>
                   </Link>
+                  <button onClick={vaciarCarrito}>VaciarCarrito</button>
+                  <button onClick={generarOrden}>Finalizar compra</button>
                   
-                </div>
-                
-              
-                <button onClick={vaciarCarrito}>VaciarCarrito</button> <button>Finalizar compra</button>
+
+                  
+                </div>  
               </div>
             </>  
         }
